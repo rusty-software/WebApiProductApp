8f913b233aaffcb04a411253d16bf758dad7ad91
Rusty B <mycoding.alert@gmail.com>
2015-04-21 20:21:00 -0500

    Adding product model and controller

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiProductApp.Models;
+
+namespace WebApiProductApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product{ Id = 1, Name = "Shoes", Category = "Footwear", Price = 39.95m },
+            new Product{ Id = 2, Name = "Glasses", Category = "Accessories", Price = 27.00m },
+            new Product{ Id = 1, Name = "Sambuca", Category = "Libation", Price = 42.50m },
+        };
+
+        IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
